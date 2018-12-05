6146d772bf39652d3a8b5651eacb07064f7d427e
zipit145 <gregbforney@gmail.com>
2018-12-05 11:43:26 -0700

    get by id working

--- queries/student_queries.js
+++ queries/student_queries.js
@@ -3,5 +3,8 @@ const db = require('../database-connection')
 module.exports = {
     readAllStudents(){
         return db('students')
+    },
+    readStudentById(id){
+        return db('students').where('id',id).first()
     }
 }
--- students.js
+++ students.js
@@ -3,7 +3,10 @@ var router =  express.Router()
 const queries = require('./queries/student_queries')
 
 router.get('/', (req,res) => {
-    queries.readAllStudents().then(students => res.status(200).send({students}))
+    queries.readAllStudents().then(students => res.status(200).send({ students }))
+})
+router.get('/:id', (req,res) => {
+    queries.readStudentById(req.params.id).then(student => res.status(200).send({ student }))
 })
 
 module.exports = router
